ca124d46cbb310c95a1ac6171cfd94413be62623
Leocvn <leo.cauvin50@gmail.com>
2021-01-26 11:19:21 +0100

    verif si on retombe sur master key : Non

--- enc_test.py
+++ enc_test.py
@@ -1,4 +1,4 @@
-from aes import AES
+from aes import AES, bytes2matrix
 import os
 
 def main():
@@ -11,16 +11,34 @@ def main():
     plain_text = aes_tool.decrypt_block(cipher_text)
     assert plain_text == b'aaaaaaaaaaaaaaaa'
 
-    # Reverse expand key
-    print("Master Key : " + str(master_key) + "\n")
-
-    i = 10
-    for round_key in AES.inverse_expand_key(aes_tool, master_key):
-        print ("Round " + str(i) + str(round_key) + "\n")
-        i-=1    
-
     # Expand key
-    AES._expand_key(aes_tool, master_key)
+    all_keys = AES._expand_key(aes_tool, master_key)
+    print(str(all_keys) + "\n")                     # Affiche toutes les clés
+    k10 = all_keys[-1]                              # On récupère la dernière clé (k10)
+    print("k10 : " + str(k10))                      # On l'affiche
+    print("type k10 : " + str(type(k10)))           # On affiche son type
+    print("k10 passe du type 'list of bytes' à 'bytes'")
+    k10 = b''.join(k10)                             # On passe k10 en bytes
+    print("k10 : " + str(k10))                      # On affiche le nouveau k10
+    print("type k10 : " + str(type(k10)) + "\n")    # On affiche son type pour vérifier qu'il est bien passé en bytes
+
+    
+    print("Master Key : " + str(master_key))
+    print("type master key" + str(type(master_key)) + "\n")
+
+    # Reverse expand key
+    i = 10
+    for round_key in AES.inverse_expand_key(aes_tool, k10):
+        print ("Round " + str(i) + " : " + str(round_key) + "\n")
+        i-=1  
+    print("First Key : " + str(round_key)) 
+    round_key=bytes2matrix(round_key)
+    print(round_key)  
+
+    print("Master Key : " + str(master_key))
+    master_key=bytes2matrix(master_key)
+    print(master_key)
+
 
 
 if __name__ == "__main__":
